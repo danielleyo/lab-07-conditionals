83ae6b7ee64295a97f71764da91c41a475bea8d1
danielleyo <69905247+danielleyo@users.noreply.github.com>
2020-10-24 21:46:33 -0400

    Add files via upload

---
+++ spanish.py
@@ -0,0 +1,9 @@
+usertime = float(input("Tell me a word in english: "))
+if usertime = cat:
+    print("gato")
+elif usertime = dog:
+    print("perro")
+elif usertime = horse:
+    print("caballo")
+else
+    print("no entiendo")
